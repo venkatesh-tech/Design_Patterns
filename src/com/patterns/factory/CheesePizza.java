35ff5dbdccceee46e782696c0f2fcfe9d47cbf16
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-02 21:19:32 +0530

    Added Factory pattern

---
+++ src/com/patterns/factory/CheesePizza.java
@@ -0,0 +1,20 @@
+package com.patterns.factory;
+
+public class CheesePizza implements Pizza {
+
+	@Override
+	public void prepare() {
+		System.out.println("Preparing Cheese Pizza");
+	}
+
+	@Override
+	public void bake() {
+		System.out.println("Baking Cheese Pizza");
+	}
+
+	@Override
+	public void cut() {
+		System.out.println("Cutting Cheese Pizza");
+	}
+
+}
---
+++ src/com/patterns/factory/Pizza.java
@@ -0,0 +1,10 @@
+package com.patterns.factory;
+
+public interface Pizza {
+
+	void prepare();
+
+	void bake();
+
+	void cut();
+}
