d5674b2a0d9613dfa30ce215a211bab55e7a30e0
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-02 13:36:28 +0530

    Added Multithreading

--- src/com/patterns/singleton/DateUtil.java
+++ src/com/patterns/singleton/DateUtil.java
@@ -2,16 +2,7 @@ package com.patterns.singleton;
 
 public class DateUtil {
 
-	// Eager intilization -- Method -2
-//	private static DateUtil instance = new DateUtil(); // step 3 
-	
-	private static DateUtil instance;
-	
-	// Static method blocks intilization -- Method -3
-	// static blocks in java are executed when class is loaded into the memory 
-	static {
-		instance = new DateUtil();
-	}
+	private static DateUtil instance; // step 3
 
 	private DateUtil() { // step 1
 
@@ -19,11 +10,13 @@ public class DateUtil {
 
 	public static DateUtil getInstance() { // step 2
 
-		// Lazy Initialization -- Method -1
-//		if (instance == null) { // stpe 5                  
-//			instance = new DateUtil(); // step 4
-//		}
-		
+		if (instance == null) {
+			synchronized (DateUtil.class) { // Handling multiple threads
+				if (instance == null) { // stpe 5 // Lazy Initialization
+					instance = new DateUtil(); // step 4
+				}
+			}
+		}
 
 		return instance;
 	}
