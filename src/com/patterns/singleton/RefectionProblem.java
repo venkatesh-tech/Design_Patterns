0ef1b64b41127487ba916037f0fd6b4fd1bf8ecb
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-02 15:31:03 +0530

    Added Reflection Problem

---
+++ src/com/patterns/singleton/RefectionProblem.java
@@ -0,0 +1,27 @@
+package com.patterns.singleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class RefectionProblem {
+
+	public static void main(String[] args) {
+		DateUtil instance1 = DateUtil.getInstance();
+		DateUtil instance2 = null;
+
+		Constructor[] declaredConstructor = DateUtil.class.getDeclaredConstructor();
+		for (Constructor constructor : declaredConstructor) {
+			constructor.setAccessible(true);
+			try {
+				instance2 = (DateUtil) constructor.newInstance(args);
+				break;
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(instance1.hashCode());
+		System.out.println(instance2.hashCode());
+	}
+
+}
