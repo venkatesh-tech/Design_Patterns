762ee9ae757b73e3c8e208dc099009a0dbccf3d8
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-07 09:44:16 +0530

    Added Template <ethod

---
+++ src/com/patterns/templatemethod/DataRenderer.java
@@ -0,0 +1,16 @@
+package com.patterns.templatemethod;
+
+public abstract class DataRenderer {
+	
+	public void render() {
+		String data = readData();
+		String processedData = processData(data);
+		System.out.println(processedData);
+	}
+	
+	public abstract String readData();
+	
+	public abstract String processData(String data);
+	
+
+}
---
+++ src/com/patterns/templatemethod/XMLDataRenderer.java
@@ -0,0 +1,17 @@
+package com.patterns.templatemethod;
+
+public class XMLDataRenderer extends DataRenderer {
+
+	@Override
+	public String readData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String processData(String data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
